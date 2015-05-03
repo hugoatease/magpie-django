bb75386e1181408a4885f39ad9f32c15eea53888
Hugo Caille <hugo.caille@orange.fr>
2015-05-03 23:40:57 +0200

    Setting up Django i18n middleware

--- magpie/settings.py
+++ magpie/settings.py
@@ -57,9 +57,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 )
 
 INSTALLED_APPS = (
