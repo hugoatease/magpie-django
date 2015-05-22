9aa5b5a417f6b09127c141221c6153290f192f49
Hugo Caille <hugo.caille@orange.fr>
2015-05-22 23:35:08 +0200

    Account signup tests

--- magpie/account/tests.py
+++ magpie/account/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.utils.crypto import get_random_string
+from models import Invite
+
 
 class AccountTestCase(TestCase):
     fixtures = ['user.json']
@@ -54,3 +57,38 @@ class AccountTestCase(TestCase):
             'email': 'holy@example.com'
         })
         self.assertEquals(200, response.status_code)
+
+
+class SignupTestCase(TestCase):
+    fixtures = ['user.json']
+
+    def test_begin(self):
+        url = reverse('account_signup_begin')
+        response = self.client.get(url)
+        self.assertEquals(200, response.status_code)
+
+        response = self.client.post(url, {'email': 'admin@example.com'})
+        self.assertEquals(200, response.status_code)
+
+        response = self.client.post(url, {'email': 'holy@example.com'})
+        self.assertEquals(200, response.status_code)
+
+    def test_wrong_invite(self):
+        url = reverse('account_signup', kwargs={'token': 'foals'})
+        response = self.client.get(url)
+        self.assertRedirects(response, reverse('account_login'), fetch_redirect_response=False)
+
+    def test_signup(self):
+        token = get_random_string(length=50)
+        Invite(email='holy@example.com', token=token).save()
+
+        url = reverse('account_signup', kwargs={'token': token})
+        response = self.client.post(url, {
+            'username': 'holy',
+            'password1': 'fire',
+            'password2': 'fire',
+            'first_name': 'Holy',
+            'last_name': 'Fire'
+        })
+
+        self.assertRedirects(response, reverse('index'), fetch_redirect_response=False)
