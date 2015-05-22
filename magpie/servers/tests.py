2d6a6c906eafc4e03ce0593166df48f264115b28
Hugo Caille <hugo.caille@orange.fr>
2015-05-22 20:15:49 +0200

    Backend connection tests

--- magpie/servers/tests.py
+++ magpie/servers/tests.py
@@ -4,16 +4,14 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from models import Server
+from magpie.models import VPNApplication
 
 
 class ServerTestCase(TestCase):
-    fixtures = ['servers.json']
-
-    def setUp(self):
-        User.objects.create_user("test", password="test")
+    fixtures = ['user.json', 'servers.json']
 
     def login(self):
-        self.client.login(username="test", password="test")
+        self.client.login(username="admin", password="admin")
 
     def test_list(self):
         url = reverse('servers_servers')
@@ -60,3 +58,21 @@ class ServerTestCase(TestCase):
 
         server = Server.objects.get(name='magpie')
         self.assertEquals(render_to_string('servers/ca.pem', {'server': server}), response.content)
+
+
+class BackendAPITestCase(APITestCase):
+    fixtures = ['user.json', 'oauth.json', 'oauth_tokens.json', 'servers.json', 'management.json']
+
+    def setUp(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer QNj05qG3Y1WXUkntMg0kQXOjCYdaa2')
+
+    def test_connect(self):
+        url = reverse('server-connect', kwargs={'name': 'magpie'})
+        response = self.client.post(url, {
+            'username': 'admin',
+            'token': 'ZTU3pXIOJMTuWatX0amX',
+            'origin': '8.8.8.8'
+        })
+
+        self.assertEquals(200, response.status_code)
+        self.assertJSONEqual(response.content, {"mask": "255.255.0.0", "type": "TUN", "address": "10.8.0.9"})
