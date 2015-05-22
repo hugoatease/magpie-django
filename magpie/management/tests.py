8e38aa08307ce63b841e01cb3217674b42b70fac
Hugo Caille <hugo.caille@orange.fr>
2015-05-22 21:47:38 +0200

    Tests for logs and bandwidth accounting

--- magpie/management/tests.py
+++ magpie/management/tests.py
@@ -22,3 +22,35 @@ class ManagementTestCase(TestCase):
         url = reverse('management_access')
         response = self.client.post(url, {'id': 1})
         self.assertEquals(200, response.status_code)
+
+    def test_access_config(self):
+        url = reverse('management_getconfig', kwargs={'name': 'magpie'})
+        response = self.client.post(url, {'id': 1})
+        self.assertEquals(200, response.status_code)
+        self.assertEquals('application/octet-stream', response['Content-Type'])
+        self.assertEquals('attachment', response['Content-Disposition'])
+
+    def test_bandwidth_report(self):
+        url = reverse('management_bandwidth')
+        response = self.client.get(url)
+        self.assertEquals(200, response.status_code)
+
+    def test_logs(self):
+        url = reverse('management_logs')
+        response = self.client.get(url)
+        self.assertEquals(200, response.status_code)
+
+    def test_logs_clear(self):
+        url = reverse('management_logs')
+        response = self.client.post(url, {'clear_history': 1})
+        self.assertEquals(200, response.status_code)
+
+    def test_logs_optout(self):
+        url = reverse('management_logs')
+        response = self.client.post(url, {'optout': 1})
+        self.assertEquals(200, response.status_code)
+
+    def test_logs_optin(self):
+        url = reverse('management_logs')
+        response = self.client.post(url, {'optout': 0})
+        self.assertEquals(200, response.status_code)
